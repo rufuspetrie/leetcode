5049d7126529b9e047518ad9296e6c531abb045a
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2023-06-03 22:49:19 -0400

    66

--- 66.py
+++ 66.py
@@ -3,20 +3,18 @@
     # need to condition on whether idx equals length of digits
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        idx = 0
+        idx = len(digits) - 1
         carry = 1
-        digits = digits[::-1]
-
         while carry:
-            if idx < len(digits):
+            if idx >= 0:
                 if digits[idx] == 9:
                     digits[idx] = 0
                 else:
                     digits[idx] += 1
                     carry = 0
             else:
-                digits.append(1)
+                digits = [1] + digits
                 carry = 0
-            idx += 1
+            idx -= 1
 
-        return digits[::-1]
+        return digits
