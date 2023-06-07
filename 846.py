c94c6fa38d4da10adafca79faa41076c0cfba6a2
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2023-06-07 18:54:40 -0400

    846

---
+++ 846.py
@@ -0,0 +1,29 @@
+# Initial thoughts
+    # notice that there can be duplicate cards like in example 1,
+        # so you can't just use a heap
+    # need counter for the number of cards, heap to track rank
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0: return False
+        
+        count = {}
+        for i in hand:
+            if i in count.keys(): count[i] += 1
+            else: count[i] = 1
+
+        heap = list(count.keys())
+        heapify(heap)
+
+        while heap:
+            card = heap[0]
+            for i in range(card, card + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                
+                if count[i] == 0:
+                    if i != heap[0]:
+                        return False
+                    heappop(heap)
+
+        return True
