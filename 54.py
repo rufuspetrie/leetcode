7a7e6a073aa8f0a401571ecbedd198571d584f70
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2023-06-07 22:30:08 -0400

    54

---
+++ 54.py
@@ -0,0 +1,32 @@
+# Initial thoughts
+    # Can just go in order while left < right, top < bottom
+    # Need break statement in the middle to make sure we didn't finish
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        top = 0
+        left = 0
+        bottom = len(matrix)
+        right = len(matrix[0])
+        
+        while left < right and top < bottom:
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+
+        return res
