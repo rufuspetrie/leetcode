c1d4dea3bcf06ba13b1234369cc215dda4774e61
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2023-06-08 23:55:20 -0400

    744

---
+++ 744.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l = 0
+        r = len(letters) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if letters[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        if l == len(letters): return letters[0]
+        else: return letters[l]
