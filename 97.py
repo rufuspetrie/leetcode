0eef4f54a608f63251f4623173b86ca18ab7b6e1
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2023-06-12 00:18:27 -0400

    97

--- 97.py
+++ 97.py
@@ -5,6 +5,12 @@
     # At each index (i,j), we can select s1[i+1] or s2[j+1] to add to the string
     # Therefore, can visualize the DP problem as a decision tree where you
         # shift one of the two pointers based on equality
+# Algorithm
+    # Use top down 2d-DP where (i,j) represents the index of each respective string
+    # If we can match the character at s1[i]/s2[j] to s3[i+j], we can mark that
+        # cell true and continue iterating backwards
+    # This lets us attempt different series of interleavings for equilvalent
+        # characters when traversing the two strings
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3): return False
@@ -18,4 +24,4 @@ class Solution:
                 if j < len(s2) and s2[j] == s3[i + j] and DP[i][j + 1]:
                     DP[i][j] = True
 
-        return DP[i][j]
+        return DP[0][0]
