f66e0cfdb991e0b2f3d4cdae6ceb4b4ea396778e
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2023-06-11 20:12:53 -0400

    43

--- 43.py
+++ 43.py
@@ -1,3 +1,22 @@
+# Initial thoughts
+    # Probably want to work index-by-index and convert each digit manually
+    # Reversing the numbers might also make it easier
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        if num1 == "0" or num2 == "0": return "0"
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        res = [0 for i in range(len(num1) + len(num2))]
+        
+        for x in range(len(num1)):
+            for y in range(len(num2)):
+                digit = (int(num1[x]) * int(num2[y]))
+                res[x + y] += digit
+                res[x + y + 1] += res[x + y] // 10
+                res[x + y] %= 10
+
+        res = res[::-1]
+        idx = 0
+        while idx < len(res) and res[idx] == 0: idx += 1
+        return "".join(map(str, res[idx:]))
