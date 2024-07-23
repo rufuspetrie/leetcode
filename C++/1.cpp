f9dc4377cf72fe261cdb4159f784c85102869983
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2024-07-23 10:47:28 -0400

    1

--- C++/1.cpp
+++ C++/1.cpp
@@ -1,12 +1,19 @@
+/*
+- Note: better to iterate through in one pass than create map first because
+- map.end() can't be compared to an int, so can't check for unique indexes
+*/
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++){
-            for (int j = 0; j < nums.size(); j++){
-                if (i != j & nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < n; i++){
+            int c = target - nums[i];
+            if(mp.find(c) != mp.end()){
+                return {mp[c], i};
             }
+            mp.insert({nums[i], i});
         }
         return {};
     }
