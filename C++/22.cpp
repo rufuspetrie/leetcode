bab5d4dcbe70fb6d772595be00a6a22bb429ee7b
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2024-07-31 02:19:01 -0400

    22

---
+++ C++/22.cpp
@@ -0,0 +1,22 @@
+/*
+- Conditions
+    - Can add open parenthesis while # < n
+    - Can add closed parentheses while # open > # closed
+*/
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        generate(n, 0, 0, "", result);
+        return result;
+    }
+private:
+    void generate(int n, int open, int close, string str, vector<string>& result){
+        if(open == n && close == n){
+            result.push_back(str);
+            return;
+        }
+        if(open < n) generate(n, open + 1, close, str + '(', result);
+        if(open > close) generate(n, open, close + 1, str + ')', result);
+    }
+};
